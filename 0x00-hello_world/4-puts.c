b6d595a7120e7b9997e68454ccd35e9d2ac212f0
wisdom <eliwisdom05@gmail.com>
2023-02-16 17:38:57 -0800

    add

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - A programming is like building a multilingual puzzle
+ * main -  programming is like building a multilingual puzzle
  * Return: 0
  */
 int main(void)
