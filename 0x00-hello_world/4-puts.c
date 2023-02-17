f67bcf602c176303a14f26dd570281b4f020925b
wisdom <eliwisdom05@gmail.com>
2023-02-16 17:57:48 -0800

    add

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A programming is like building a multilingual puzzle(SUCCESS)
+ * return: 0 (SUCCESS)
+ */
+int main(void)
+{
+put("\"Programming is like building a multilingual puzzle");
+return (0);
+}
