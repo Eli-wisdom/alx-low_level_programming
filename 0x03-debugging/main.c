cbac80b9576f07292f0a91cc5683b1b648bed018
wisdom <eliwisdom05@gmail.com>
2023-02-22 07:58:30 -0800

    task 0

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - testing function that displays if integer is positive or negative
+ * Return: 0 (SUCCESS)
+**/
+
+int main(void)
+{
+	 int i;
+
+	  i = 98;
+	   positive_or_negative(i);
+
+	    return (0);
+}
