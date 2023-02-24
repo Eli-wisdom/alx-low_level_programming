80c76ee2e9f92f0bfd8987636a6c214587ee2613
wisdom <eliwisdom05@gmail.com>
2023-02-23 17:46:44 -0800

    mandatory

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0-9
+ * return: void
+ **/
+
+void print_numbers(void)
+{
+char ch;
+for (ch = '0'; ch <= '9'; ch++)
+{putchar(ch);
+}
+_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print num 0-9
+ * except 2 and 4
+ *
+ * return: void
+ **/
+
+void print_most_numbers(void)
+{
+char ch;
+
+for (ch = '0'; ch <= '9'; ch++)
+{
+if (!(ch == '2' || ch == '4'))
+_putchar(ch);
+}
+_putchar('\n');
+}
