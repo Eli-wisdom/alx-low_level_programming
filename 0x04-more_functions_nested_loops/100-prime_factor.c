cf0b4485848a0118d35f8b20f4448a6df613269f
wisdom <eliwisdom05@gmail.com>
2023-02-23 18:25:09 -0800

    add

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,33 +8,29 @@
  */
 int main(void)
 {
-	long int n;
-	long int max;
-	long int x;
+long int n;
+long int max;
+long int x;
 
-	n = 612852475143;
-	max = -1;
-
-	while (n % 2 == 0)
-	{
-		max = 2;
-		n /= 2;
-	}
-
-	for (x = 3; x <= sqrt(n); x = x + 2)
-	{
-		while (n % x == 0)
-		{
-			max = x;
-			n = n / x;
-		}
-	}
-
-	if (n > 2)
-		max = n;
-
-	printf("%ld\n", max);
-
-	return (0);
+n = 612852475143;
+max = -1;
+while (n % 2 == 0)
+{
+max = 2;
+n /= 2;
 }
-
+for (x = 3; x <= sqrt(n); x = x + 2)
+{
+while (n % x == 0)
+{
+max = x;
+n = n / x;
+}
+}
+if (n > 2)
+{
+max = n;
+printf("%ld\n", max);
+return (0);
+}
+}
