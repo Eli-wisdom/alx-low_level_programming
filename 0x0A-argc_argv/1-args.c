94de0d8b1db64b820484c6cb011790ea60ebf6f4
wisdom <eliwisdom05@gmail.com>
2023-03-15 04:00:15 -0700

    add

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,18 +10,15 @@ nclude <stdio.h>
  **/
 
 int main(int argc, char *argv[])
+			
 {
-int i;
+			
+	(void) argvi;
+			
+		printf("%d\n", argc - 1);
+				
 
-	if (argc == 1)
-				printf("%d\n", argc - 1);
-		else
-				{
-							for (i = 0; *argv; i++, argv++)
-											;
-
-									printf("%d\n", i - 1);
-										}
-
+				
 			return (0);
+					
 }
